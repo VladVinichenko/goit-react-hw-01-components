0ab17dabbc6dbb0ca9abd8cf4ef3c7c060d97926
VladVinichenko <vinnyxxxl925@gmail.com>
2022-01-20 19:41:27 +0200

    complete v1

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,35 +1,44 @@
-// import propTypes from "prop-types";
+import propTypes from "prop-types";
 import s from './Profile.module.css'
-const Profile = () => {
+const Profile = ({ avatar, username, tag, location, stats }) => {
   return (
     <div className={s.profile}>
-      <div className="s.description">
+      <div className={s.description}>
         <img
-          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+          // src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
           alt="User avatar"
-          className="s.avatar"
+          src={avatar}
+          className={s.avatar}
         />
-        <p className="s.name">Petra Marica</p>
-        <p className="s.tag">@pmarica</p>
-        <p className="s.location">Salvador, Brasil</p>
+        <p className={s.username}>{username}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
       </div>
 
-      <ul className="s.stats">
-        <li>
-          <span className="s.label">Followers</span>
-          <span className="s.quantity">1000</span>
+      <ul className={s.stats}>
+        <li className={s.statsItem}>
+          <span className={s.label}>Followers</span>
+          <span className={s.quantity}>{stats.followers}</span>
         </li>
-        <li>
-          <span className="s.label">Views</span>
-          <span className="s.quantity">2000</span>
+        <li className={s.statsItem}>
+          <span className={s.label}>Views</span>
+          <span className={s.quantity}>{stats.views}</span>
         </li>
-        <li>
-          <span className="s.label">Likes</span>
-          <span className="s.quantity">3000</span>
+        <li className={s.statsItem}>
+          <span className={s.label}>Likes</span>
+          <span className={s.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
   )
+}
+
+Profile.propTypes = {
+  avatar: propTypes.string.isRequired,
+  username: propTypes.string.isRequired,
+  tag: propTypes.string.isRequired,
+  location: propTypes.string.isRequired,
+  stats: propTypes.object.isRequired,
 }
 
 export default Profile
